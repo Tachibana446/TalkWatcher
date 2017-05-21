e61cc9191d4a9c7a0047ac4b363b4a66d520de23
Tachibana446 <stinger33z@gmail.com>
2017-05-21 23:31:48 +0900

    [add]たぶんできた

--- content.js
+++ content.js
@@ -1,1 +1,23 @@
-console.log("content.js");
+window.onload = () => {
+    console.log("content.js");
+    $('.talk-element').each((i, li) => {
+        var title = $(li).find('.element-title > a')
+        var p = $(li).find('.element-comment')
+        var commentText = $(p).text().replace(/コメント/, "")
+        var comment = parseInt(commentText, 10)
+        setNewMessage(title, comment)
+    })
+}
+
+function setNewMessage(title, num) {
+    chrome.storage.sync.get($(title).text(), (value) => {
+        //debugger;
+        if (Object.keys(value).length === 0 || value < num) {
+            // New
+            var data = {}
+            data[$(title).text()] = num
+            chrome.storage.sync.set(data)
+            $(title).prepend('【New!】')
+        }
+    })
+}
